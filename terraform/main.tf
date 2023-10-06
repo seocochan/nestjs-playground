d8a316fa7efcb509b72140a35365e3d3e0381d34
seocochan <seokho.dev@gmail.com>
2023-10-06 15:33:58 +0900

    feat: add EventBridge Scheduler

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,9 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "example-tfstate"
-    key            = "terraform.tfstate"
-    region         = "ap-northeast-2"
+    bucket = "example-tfstate"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-2"
   }
 }
 
@@ -88,6 +88,56 @@ resource "aws_iam_role_policy_attachment" "aws_ecs_task_execution_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_iam_policy" "scheduler_batch_policy" {
+  name = "tf_scheduler_batch_policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "batch:SubmitJob",
+          "batch:DescribeJobs",
+          "batch:TerminateJob"
+        ],
+        "Resource" : "*",
+        "Effect" : "Allow"
+      },
+      {
+        "Action" : [
+          "events:PutTargets",
+          "events:PutRule",
+          "events:DescribeRule"
+        ],
+        "Resource" : [
+          "*"
+        ],
+        "Effect" : "Allow"
+      }
+    ]
+    }
+  )
+}
+
+resource "aws_iam_role" "scheduler_batch_role" {
+  name                = "tf_scheduler_batch_role"
+  managed_policy_arns = [aws_iam_policy.scheduler_batch_policy.arn]
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
 resource "aws_batch_compute_environment" "batch" {
   compute_environment_name = "tf_example_compute_env"
 
@@ -97,7 +147,7 @@ resource "aws_batch_compute_environment" "batch" {
       aws_security_group.batch.id,
     ]
     subnets = data.aws_subnets.all_default_subnets.ids
-    type = "FARGATE"
+    type    = "FARGATE"
   }
   service_role = aws_iam_role.aws_batch_service_role.arn
   type         = "MANAGED"
@@ -142,4 +192,28 @@ resource "aws_batch_job_definition" "batch" {
     ]
     executionRoleArn = aws_iam_role.aws_ecs_task_execution_role.arn
   })
+}
+
+resource "aws_scheduler_schedule" "batch" {
+  name  = "tf_batch_submit_job_schedule"
+  state = "ENABLED"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression          = "rate(5 minutes)"
+  schedule_expression_timezone = "Asia/Seoul"
+  description                  = "submitJob Batch event"
+
+  target {
+    arn      = "arn:aws:scheduler:::aws-sdk:batch:submitJob"
+    role_arn = aws_iam_role.scheduler_batch_role.arn
+
+    input = jsonencode({
+      "JobName" : "scheduled_job",
+      "JobDefinition" : replace(aws_batch_job_definition.batch.arn, "/:\\d+$/", ""),
+      "JobQueue" : aws_batch_job_queue.batch.arn
+    })
+  }
 }
