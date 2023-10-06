1bdfb53ae5bad7c49b7ca44460aa780f08c66cfc
seocochan <seokho.dev@gmail.com>
2023-10-06 15:32:38 +0900

    feat: add args for command

--- apps/batch/src/batch.command.ts
+++ apps/batch/src/batch.command.ts
@@ -1,12 +1,13 @@
 import { Command, CommandRunner } from 'nest-commander';
 import { BatchService } from './batch.service';
 
-@Command({ name: 'batch' })
+@Command({ name: 'batch', arguments: '<message>' })
 export class BatchCommand extends CommandRunner {
   constructor(private readonly service: BatchService) {
     super();
   }
-  async run() {
-    console.log(this.service.getHello());
+  async run(inputs: string[]) {
+    const [message] = inputs
+    console.log(this.service.getHello() + ' ' + message);
   }
 }
